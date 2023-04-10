7467142e5579a162515d4506d7c6a922a189ef4a
Oleksiy Khomenko <OKhomenko@aevex.com>
2023-04-09 20:30:54 -0700

    saving/loading ingredients from file

--- recipe_book/lib/ingredients_tab.dart
+++ recipe_book/lib/ingredients_tab.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import "dart:io";
 
 enum Units { ml, gm, oz, floz }
 
@@ -7,46 +11,317 @@ class IngredientObject {
   Units unit;
   double quantity;
   double price;
+  double pricePerUnit;
+
+  static List<String> get labels => ["Name", "Price Per Unit", "Unit"];
+
+  List<String> get values => [name, pricePerUnit.toStringAsFixed(2), unit.name];
 
   IngredientObject(
       {required this.name,
       required this.unit,
       required this.quantity,
-      required this.price});
+      required this.price})
+      : pricePerUnit = (price == 0 || quantity == 0) ? 0 : price / quantity;
+
+  bool sameAs(IngredientObject other) {
+    return name == other.name &&
+        unit == other.unit &&
+        quantity == other.quantity &&
+        price == other.price;
+  }
+
+  Map<String, dynamic> toJson() =>
+      {"name": name, "unit": unit.name, "quantity": quantity, "price": price};
+
+  static IngredientObject? fromJson(Map<String, dynamic> json) {
+    var n = json["name"];
+    int? u = Units.values.indexWhere((element) => element.name == json["unit"]);
+    if (u < 0) u = null;
+    var q = json["quantity"];
+    var p = json["price"];
+    if ([n, u, q, p].contains(null)) {
+      return null;
+    }
+    return IngredientObject(
+        name: n, price: p!, quantity: q!, unit: Units.values[u!]);
+  }
 }
 
-class IngredientsTab extends StatelessWidget {
-  IngredientsTab({super.key});
-  final List<IngredientObject> ingredientsList = [];
-  final ValueNotifier<bool> _rebuildList = ValueNotifier(false);
+class IngredientsList extends ChangeNotifier {
+  static const String jsonName = "ingredientsList";
+  final List<IngredientObject> ingList = [];
 
-  void addIngredient(IngredientObject newIng) {
-    if (!ingredientsList.contains(newIng)) {
-      ingredientsList.add(newIng);
-      _rebuildList.value = !_rebuildList.value;
+  int get length => ingList.length;
+  bool get isEmpty => ingList.isEmpty;
+
+  bool modified = false;
+  bool saving = false;
+
+  IngredientObject operator [](int i) => ingList[i];
+
+  operator []=(int i, IngredientObject value) {
+    if (value != ingList[i]) {
+      ingList[i] = value;
+      notifyListeners();
     }
   }
 
-  void removeIngredient(IngredientObject newIng) {
-    if (ingredientsList.contains(newIng)) {
-      ingredientsList.remove(newIng);
-      _rebuildList.value = !_rebuildList.value;
+  void add(IngredientObject newIng, {bool isLoadedValue = false}) {
+    if (ingList
+        .where(
+          (element) => element.name == newIng.name,
+        )
+        .isEmpty) {
+      ingList.add(newIng);
+      if (!isLoadedValue) {
+        notifyListeners();
+        modified = true;
+      }
     }
+  }
+
+  void addAll(List<IngredientObject> newList) {
+    for (var el in newList) {
+      add(el, isLoadedValue: true);
+    }
+    notifyListeners();
+  }
+
+  void remove(IngredientObject newIng) {
+    if (ingList.contains(newIng)) {
+      ingList.remove(newIng);
+      notifyListeners();
+      modified = true;
+    }
+  }
+
+  Map<String, dynamic> toJson() =>
+      {jsonName: List.generate(length, (index) => ingList[index].toJson())};
+
+  static List<IngredientObject> ingredientsFromJson(Map<String, dynamic> json) {
+    List<dynamic> iList = json[jsonName] ?? [];
+    List<IngredientObject> retVal = [];
+    for (var el in iList) {
+      IngredientObject? ing = IngredientObject.fromJson(el);
+      if (ing != null) {
+        retVal.add(ing);
+      }
+    }
+    return retVal;
+  }
+}
+
+class IngredientsTab extends StatelessWidget {
+  static const String savingFolderName = "SaveData";
+  static const String ingredientsFileName = "SavedIngredients";
+  final File ingFile = File(
+      "${Directory.current.path}/$savingFolderName/$ingredientsFileName.json");
+
+  IngredientsTab({super.key}) {
+    Timer.periodic(const Duration(seconds: 60), (timer) {
+      if (ingredientsList.modified &&
+          !ingredientsList.isEmpty &&
+          !ingredientsList.saving) {
+        saveToFile();
+      }
+    });
+  }
+
+  final IngredientsList ingredientsList = IngredientsList();
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  void addIngredient(IngredientObject newIng) {
+    ingredientsList.add(newIng);
+  }
+
+  void removeIngredient(IngredientObject newIng) {
+    ingredientsList.remove(newIng);
+  }
+
+  Future loadFromFile() async {
+    if (await ingFile.exists()) {
+      String fileContent = await ingFile.readAsString();
+      var loadedList =
+          IngredientsList.ingredientsFromJson(jsonDecode(fileContent));
+      ingredientsList.ingList.addAll(loadedList);
+    }
+  }
+
+  Future saveToFile() async {
+    ingredientsList.saving = true;
+    File? oldFile;
+    if (await ingFile.exists()) {
+      oldFile = await ingFile.rename("${ingredientsFileName}_old.json");
+    } else {
+      await ingFile.create(recursive: true);
+    }
+
+    await ingFile.writeAsString(jsonEncode(ingredientsList.toJson()));
+    await oldFile?.delete();
+    ingredientsList.modified = false;
+    ingredientsList.saving = false;
+  }
+
+  Widget editIngredientDialog(BuildContext context, IngredientObject? ingObj) {
+    Units? u = ingObj?.unit;
+    double? q = ingObj?.quantity;
+    double? p = ingObj?.price;
+    String? n = ingObj?.name;
+    return Dialog(
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        height: 400,
+        width: 150,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Cancel"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState?.validate() == true) {
+                      formKey.currentState?.save();
+                      Navigator.of(context).pop(IngredientObject(
+                          name: n!, unit: u!, quantity: q!, price: p!));
+                    }
+                  },
+                  child: const Text("Save"),
+                ),
+              ],
+            ),
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null) {
+                        return "Name is required";
+                      }
+                    },
+                    onSaved: (value) {
+                      n = value;
+                    },
+                    initialValue: n,
+                    autofocus: true,
+                    decoration: const InputDecoration(hintText: "Name"),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null) {
+                              return "Price is required";
+                            }
+                            double? newP = double.tryParse(value!);
+                            if (newP == null || newP < 0) {
+                              return "Has to be real, positive number";
+                            }
+                          },
+                          onSaved: (value) {
+                            p = double.parse(value!);
+                          },
+                          decoration: const InputDecoration(hintText: "Price"),
+                          initialValue: p?.toString(),
+                        ),
+                      ),
+                      const Text("\$"),
+                    ],
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null) {
+                        return "Quantity is required";
+                      }
+                      double? newQ = double.tryParse(value!);
+                      if (newQ == null || newQ < 0) {
+                        return "Has to be real, positive number";
+                      }
+                    },
+                    onSaved: (value) {
+                      q = double.parse(value!);
+                    },
+                    decoration: const InputDecoration(hintText: "Quantity"),
+                    initialValue: q?.toString(),
+                  ),
+                  DropdownButtonFormField<Units>(
+                    hint: const Text("Units"),
+                    value: u,
+                    items: Units.values
+                        .asNameMap()
+                        .keys
+                        .map<DropdownMenuItem<Units>>(
+                            (e) => DropdownMenuItem<Units>(
+                                  child: Text(e),
+                                  value: Units.values.byName(e),
+                                ))
+                        .toList(),
+                    onChanged: (value) {
+                      u = value;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-        valueListenable: _rebuildList,
-        builder: (context, _, __) {
-          return ListView.builder(
-            itemCount: ingredientsList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(ingredientsList[index].name),
-              );
-            },
-          );
+    return AnimatedBuilder(
+        animation: ingredientsList,
+        builder: (context, _) {
+          return DataTable(
+              showCheckboxColumn: false,
+              headingRowHeight: 40,
+              dataRowHeight: 40,
+              headingRowColor: MaterialStatePropertyAll<Color>(
+                  Theme.of(context).colorScheme.onSecondary),
+              dataRowColor: MaterialStatePropertyAll<Color>(
+                  Colors.white.withOpacity(0.1)),
+              columns: IngredientObject.labels
+                  .map<DataColumn>((e) => DataColumn(label: Text(e)))
+                  .toList(),
+              rows: ingredientsList.ingList
+                  .map<DataRow>((ingredient) => DataRow(
+                      onSelectChanged: (_) {
+                        int index = ingredientsList.ingList.indexWhere(
+                          (element) => element.name == ingredient.name,
+                        );
+                        if (index >= 0) {
+                          showDialog<IngredientObject>(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (context) {
+                              return editIngredientDialog(context, ingredient);
+                            },
+                          ).then((value) {
+                            if (value != null &&
+                                !ingredientsList[index].sameAs(value)) {
+                              ingredientsList[index] = value;
+                              ingredientsList.modified = true;
+                            }
+                          });
+                        }
+                      },
+                      cells: ingredient.values
+                          .map<DataCell>((e) => DataCell(
+                                Text(e),
+                              ))
+                          .toList()))
+                  .toList());
         });
   }
 }
--- recipe_book/lib/main.dart
+++ recipe_book/lib/main.dart
@@ -1,40 +1,61 @@
 import 'package:flutter/material.dart';
-import "recipes_tab.dart";
 import "ingredients_tab.dart";
+import "recipes_tab.dart";
 
 void main() {
-  runApp(const MyApp());
+  IngredientsTab i = IngredientsTab();
+  RecipesTab r = RecipesTab();
+  i.loadFromFile().then(
+    (value) {
+      runApp(MyApp(
+        ingredients: i,
+        recipes: r,
+      ));
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.ingredients, required this.recipes});
+
+  final IngredientsTab ingredients;
+  final RecipesTab recipes;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Recipe Book',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: Typography.whiteRedmond,
         colorScheme: ColorScheme.fromSeed(
             seedColor: Colors.amber, brightness: Brightness.dark),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Recipe Book'),
+      home: MyHomePage(
+        ingredients: ingredients,
+        recipes: recipes,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage(
+      {super.key, required this.ingredients, required this.recipes});
 
-  final String title;
+  final IngredientsTab ingredients;
+  final RecipesTab recipes;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController mainTabController;
+
   @override
   void dispose() {
     super.dispose();
@@ -42,40 +63,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    mainTabController = TabController(length: 2, vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: TabBar(
-            padding: const EdgeInsets.all(5),
-            onTap: (value) {
-              if (DefaultTabController.of(context).index == value) {
-                if (value == 0) {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return Text("Add Recipe");
-                    },
-                  );
-                }else{
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return Text("Add Ingredient");
-                    },
-                  );
+    return Scaffold(
+      appBar: TabBar(
+        labelPadding: const EdgeInsets.all(5),
+        controller: mainTabController,
+        padding: const EdgeInsets.all(5),
+        onTap: (value) {
+          if (!mainTabController.indexIsChanging) {
+            if (value == 0) {
+              // showDialog(
+              //   context: context,
+              //   builder: (context) {
+              //     return widget.ingredients.editIngredientDialog(context, null);
+              //   },
+              // );
+            } else {
+              showDialog<IngredientObject>(
+                context: context,
+                builder: (context) {
+                  return widget.ingredients.editIngredientDialog(context, null);
+                },
+              ).then((value) {
+                if (value != null) {
+                  widget.ingredients.addIngredient(value);
                 }
-              }
-            },
-            tabs: DefaultTabController.of(context).index == 0
-                ? const [Text("Add Recipe"), Text("Ingredients")]
-                : const [Text("Recipes"), Text("Add Ingredient")],
-          ),
-          body: TabBarView(children: [RecipesTab(), IngredientsTab()]),
-        ));
+              });
+            }
+          }
+        },
+        tabs: mainTabController.index == 0
+            ? const [Text("Add Recipe"), Text("Ingredients")]
+            : const [Text("Recipes"), Text("Add Ingredient")],
+      ),
+      body: TabBarView(
+          controller: mainTabController,
+          children: [widget.recipes, widget.ingredients]),
+    );
   }
 }
